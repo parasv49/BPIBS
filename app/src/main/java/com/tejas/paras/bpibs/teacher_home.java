5b0f12ca64bd0e42a1c07cbf705652586e611310
abhaysadanandan <meabhay92@gmail.com>
2017-02-16 11:51:53 +0530

    added date

--- app/src/main/java/com/tejas/paras/bpibs/teacher_home.java
+++ app/src/main/java/com/tejas/paras/bpibs/teacher_home.java
@@ -8,15 +8,40 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.jjobes.slidedatetimepicker.SlideDateTimeListener;
+import com.github.jjobes.slidedatetimepicker.SlideDateTimePicker;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class teacher_home extends AppCompatActivity {
 
+    private SimpleDateFormat mFormatter = new SimpleDateFormat("dd-MM-yyyy");
     private TextView tv5;
     private Spinner sp1,sp2;
     private Button b4;
+
+    private SlideDateTimeListener listener = new SlideDateTimeListener() {
+
+        @Override
+        public void onDateTimeSet(Date date)
+        {
+
+            tv5.setText(mFormatter.format(date));
+        }
+
+        // Optional cancel listener
+        @Override
+        public void onDateTimeCancel()
+        {
+            Toast.makeText(teacher_home.this,
+                    "Canceled", Toast.LENGTH_SHORT).show();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +50,7 @@ public class teacher_home extends AppCompatActivity {
         sp1=(Spinner)findViewById(R.id.spinner);
         sp2=(Spinner)findViewById(R.id.spinner2);
         b4=(Button)findViewById(R.id.button4);
+
 
         List<String> course = new ArrayList<>();
         course.add("--Select--");
@@ -49,7 +75,21 @@ public class teacher_home extends AppCompatActivity {
         sp2.setAdapter(dataAdapter2);
 
 // textView is the TextView view that should display it
-        tv5.setText("yo");
+        tv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new SlideDateTimePicker.Builder(getSupportFragmentManager())
+                        .setListener(listener)
+                        .setInitialDate(new Date())
+                        //.setMinDate(minDate)
+                        //.setMaxDate(maxDate)
+                        //.setIs24HourTime(true)
+                        //.setTheme(SlideDateTimePicker.HOLO_DARK)
+                        //.setIndicatorColor(Color.parseColor("#990000"))
+                        .build()
+                        .show();
+            }
+        });
     }
     public void attendence(View v)
     {
